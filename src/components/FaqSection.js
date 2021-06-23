7045292583428665a8b717045d6e90ac793266cd
Pau Costa Ferrer <pau@micodev.es>
2021-06-23 17:52:51 +0200

    Added Styles to FAQSection

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,13 +1,16 @@
 import React from "react";
+//Styles import
+import styled from "styled-components";
+import { StyledGeneralLayout } from "../styles";
 
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <StyledFaq>
       <h2>
         Any Questions? <span>FAQ</span>
       </h2>
       <div className="question">
-        <h4>How Do i Start?</h4>
+        <h4>How Do I Start?</h4>
         <div className="answer">
           <p>Lorem ipsum dolor sit amet.</p>
           <p>
@@ -15,6 +18,7 @@ const FaqSection = () => {
             odit.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Daily Schedule</h4>
@@ -25,6 +29,7 @@ const FaqSection = () => {
             odit.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Different Payments Methods</h4>
@@ -35,6 +40,7 @@ const FaqSection = () => {
             odit.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What Products do you offer?</h4>
@@ -45,9 +51,39 @@ const FaqSection = () => {
             odit.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </StyledFaq>
   );
 };
+
+//Styled
+
+const StyledFaq = styled(StyledGeneralLayout)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;
