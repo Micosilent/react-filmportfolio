0989180fdb6a654f1a4a912f7908244363d9dcb0
Pau Costa Ferrer <pau@micodev.es>
2021-06-24 13:46:22 +0200

    Added Nav

---
+++ src/components/Nav.js
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+
+const Nav = () => {
+  return (
+    <StyledNav>
+      <h1>
+        <a id="logo" href="#">
+          Capture
+        </a>
+      </h1>
+      <ul>
+        <li>
+          <a href="#">About us</a>
+        </li>
+        <li>
+          <a href="#">Our Work</a>
+        </li>
+        <li>
+          <a href="#">Contact Us</a>
+        </li>
+      </ul>
+    </StyledNav>
+  );
+};
+
+const StyledNav = styled.nav`
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10rem;
+  background-color: #282828;
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  ul {
+    display: flex;
+    list-style: none;
+  }
+  li {
+    padding-left: 10rem;
+    position: relative;
+  }
+  #logo {
+    font-size: 2rem;
+    font-family: "lobster", cursive;
+    font-weight: lighter;
+  }
+`;
+
+export default Nav;
